8d9f146b08dae30799dc0d122b2902a40bb76c82
lgluiz1 <legalhints@gmail.com>
2025-04-29 10:50:24 -0300

    Added: Admin Panel Views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post, Comment, Reviews, Image
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Reviews)
+admin.site.register(Image)
+
+
