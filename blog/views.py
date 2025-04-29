73e8bfa721fdc83dfcf25d86a5c999ddea6f30d1
lgluiz1 <legalhints@gmail.com>
2025-04-29 11:25:39 -0300

    creation: routes api views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import PermissionDenied
+from .models import Post, Comment, Reviews, Image
+from .serializers import BlogSerializer, CommentSerializer, ReviewSerializer, ImageSerializer
 
-# Create your views here.
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Apenas leitores podem ver, mas só o superusuário pode editar/criar
+
+    def perform_create(self, serializer):
+        # Garante que apenas o superusuário pode criar posts
+        if not self.request.user.is_superuser:
+            raise PermissionDenied("Somente o superusuário pode criar posts.")
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_superuser:
+            raise PermissionDenied("Somente o superusuário pode criar comentários.")
+        serializer.save(user=self.request.user)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_superuser:
+            raise PermissionDenied("Somente o superusuário pode criar avaliações.")
+        serializer.save(user=self.request.user)
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_superuser:
+            raise PermissionDenied("Somente o superusuário pode adicionar imagens.")
+        serializer.save(user=self.request.user)
