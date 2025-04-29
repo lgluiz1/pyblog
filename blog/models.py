0c84854138d484e4e94f8caaf8790fcc2de79d3c
lgluiz1 <legalhints@gmail.com>
2025-04-29 10:51:04 -0300

    Added: blog templates, ratings, comments

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# Cria os modelos da aplicação blog
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title    
+    
+
+# Modelos de comentários
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+    
+    
+# Avaliações de posts
+class Reviews(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    stars = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.stars)
+    
+    
+# Imagens dos posts
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.image)
+
+
