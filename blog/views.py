dc4da896edd341c490936590e89cf8391b6bb308
lgluiz1 <legalhints@gmail.com>
2025-04-29 12:41:15 -0300

    authorization update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly , AllowAny
 from rest_framework.exceptions import PermissionDenied
 from .models import Post, Comment, Reviews, Image
 from .serializers import BlogSerializer, CommentSerializer, ReviewSerializer, ImageSerializer
@@ -10,30 +10,30 @@ class BlogViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]  # Apenas leitores podem ver, mas só o superusuário pode editar/criar
 
     def perform_create(self, serializer):
-        # Garantir que o 'user' seja atribuído automaticamente ao criar um novo post
+        if not self.request.user.is_superuser:
+            raise PermissionDenied("Somente o superusuário pode criar posts.")
         serializer.save(user=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
-        if not self.request.user.is_superuser:
-            raise PermissionDenied("Somente o superusuário pode criar comentários.")
-        serializer.save(user=self.request.user)
+        user = self.request.user if self.request.user.is_authenticated else None
+        serializer.save(user=user)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Reviews.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
-        if not self.request.user.is_superuser:
-            raise PermissionDenied("Somente o superusuário pode criar avaliações.")
-        serializer.save(user=self.request.user)
+        # Qualquer usuário autenticado pode criar avaliações
+        user = self.request.user if self.request.user.is_authenticated else None
+        serializer.save(user=user)
 
 
 class ImageViewSet(viewsets.ModelViewSet):
